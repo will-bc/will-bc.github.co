1e999621092ecf631bf5eb13cfceb961461e376b
will-bc <willianbarelac@gmail.com>
2025-07-24 17:54:04 +0000

    Deploying to gh-pages from @ will-bc/will-bc.github.io@c05bc111feb38c2888d268b35997ff9af0f59a90 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -444,12 +444,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-open-hardware",
-          title: 'Open Hardware',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
